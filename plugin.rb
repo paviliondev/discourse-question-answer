02e59e4ffafac4e0a33ea480dd9062c2c1eb93ee
Angus McLeod <angus@mcleod.org.au>
2017-05-05 16:20:01 -0700

    rm log

--- plugin.rb
+++ plugin.rb
@@ -41,7 +41,6 @@ after_initialize do
     def check_if_voted
       if current_user && params[:post_action_type_id].to_i === PostActionType.types[:vote] &&
         QAHelper.qa_enabled(@post.topic) && QAHelper.user_has_voted(@post.topic, current_user)
-         puts "PASSED"
          raise Discourse::InvalidAccess.new, I18n.t('vote.alread_voted')
       end
     end
