0400d6e3dea95ec6c766f960fe7ad58becd40aaa
Angus McLeod <angus@mcleod.org.au>
2018-02-27 11:28:53 +0800

    Add vote action undo time limit setting

--- plugin.rb
+++ plugin.rb
@@ -143,4 +143,30 @@ after_initialize do
       scope.current_user && QAHelper.user_has_voted(object.topic, scope.current_user)
     end
   end
+
+  module PostGuardianVoteExtension
+    def can_delete_post_action?(post_action)
+      # only use extension if post_action is a vote
+      return super(post_action) unless post_action.post_action_type_id == PostActionType.types[:vote]
+
+      # You can only undo your own actions
+      return false unless is_my_own?(post_action) && not(post_action.is_private_message?)
+
+      # Apply vote action window if it exists
+      vote_window = SiteSetting.qa_undo_vote_action_window.to_i
+      if vote_window == 0
+        return true
+      elsif vote_window.present?
+        return post_action.created_at > vote_window.minutes.ago
+      end
+
+      # Use post action setting as default
+      post_action.created_at > SiteSetting.post_undo_action_window_mins.minutes.ago
+    end
+  end
+
+  require_dependency 'guardian'
+  class ::Guardian
+    prepend PostGuardianVoteExtension
+  end
 end
