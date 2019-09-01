e81eb6a1e978aa88d47c36571d150d235f5ade68
Angus McLeod <angus@mcleod.org.au>
2019-09-01 22:28:28 +1000

    Handle multiple votes

--- lib/qa.rb
+++ lib/qa.rb
@@ -148,22 +148,24 @@ class QuestionAnswer::Vote
   def self.vote(post, user, args)
     modifier = 0
 
-    if args[:direction] === UP
-      modifier = args[:action] === CREATE ? 1 : -1
-    end
-
-    post.custom_fields['vote_count'] = post.qa_vote_count + modifier
-
     voted = post.qa_voted
 
     if args[:direction] === UP
       if args[:action] === CREATE
         voted.push(user.id)
+        modifier = 1
       elsif args[:action] === DESTROY
-        voted.delete(user.id)
+        modifier = 0
+        voted.delete_if do |user_id|
+          if user_id === user.id
+            modifier = modifier - 1
+            true
+          end
+        end
       end
     end
 
+    post.custom_fields['vote_count'] = post.qa_vote_count + modifier
     post.custom_fields['voted'] = voted
 
     votes = post.qa_vote_history
