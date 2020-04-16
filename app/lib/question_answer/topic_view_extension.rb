2baa9060f8432e677420b6ce368a4fb94ec95ee9
Muhlis <muhlisbc@gmail.com>
2020-04-16 11:58:55 +0700

    DEV: remove filter_posts_by_ids override

--- app/lib/question_answer/topic_view_extension.rb
+++ app/lib/question_answer/topic_view_extension.rb
@@ -5,23 +5,5 @@ module QuestionAnswer
     def qa_enabled
       Topic.qa_enabled(@topic)
     end
-
-    def filter_posts_by_ids(post_ids)
-      if qa_enabled
-        posts = begin
-          Post
-            .where(id: post_ids, topic_id: @topic.id)
-            .includes(:user, :reply_to_user, :incoming_email)
-        end
-        order = 'case when post_number = 1 then 0 else 1 end, sort_order ASC'
-        @posts = posts.order(order)
-        @posts = filter_post_types(@posts)
-        @posts = @posts.with_deleted if @guardian.can_see_deleted_posts?
-
-        @posts
-      else
-        super
-      end
-    end
   end
 end
