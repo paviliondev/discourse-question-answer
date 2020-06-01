26faf5bb7c57c463b567410d54975a16997fabf6
Muhlis <muhlisbc@gmail.com>
2020-06-01 18:53:59 +0700

    Version bump (1)

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-question-answer
 # about: Question / Answer Style Topics
-# version: 0.3
+# version: 1.0.0
 # authors: Angus McLeod, Muhlis Cahyono (muhlisbc@gmail.com)
 # url: https://github.com/paviliondev/discourse-question-answer
 
